ee53c78e7e3456e55f2cf8cf7fd125c73ddeae96
mostafa waliallah <mostafawaliallah@gmail.com>
2026-04-17 17:18:39 +0200

    add influx DB and Grafana image

--- 3_data_processing/streaming_consumers/sensor_consumer.py
+++ 3_data_processing/streaming_consumers/sensor_consumer.py
@@ -5,6 +5,23 @@ import joblib
 from kafka import KafkaConsumer
 from tensorflow.keras.models import load_model
 from collections import deque 
+import time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# ==========================================
+# 0. إعدادات InfluxDB (قم بتعديلها حسب سيرفرك)
+# ==========================================
+INFLUX_URL = "http://localhost:8086"
+INFLUX_TOKEN = "t4Zac1hxXZQvIIfeBCoJLJgxJwWIPDPTknBSl54o1erJHqfG3vPdr0RVZocUGIrfSppVa5nF4gXyKbxEnVJRQA==" # ضع التوكن الخاص بك هنا
+INFLUX_ORG = "kave_org"                   # اسم المنظمة
+INFLUX_BUCKET = "cnc_digital_twin"        # اسم الـ Bucket (قاعدة البيانات)
+
+# الاتصال بـ InfluxDB
+client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+print("🛢️ Connected to InfluxDB successfully!")
 
 
 model = load_model('/home/elwali/Graduation_Project/Intelligent-Manufacturing-Digital-Twin-for-Real-Time-Inspection-and-Predictive-Planning/cnc_lstm_model.keras')
@@ -65,9 +82,27 @@ for message in consumer:
             
            
             probabilities = model.predict(X_live, verbose=0)[0]
-            predicted_stage = np.argmax(probabilities) 
-            confidence = probabilities[predicted_stage] * 100
+            predicted_stage = int(np.argmax(probabilities)) 
+            confidence = float(probabilities[predicted_stage] * 100)
+            # ==========================================
+            # 🚀 إرسال البيانات إلى InfluxDB
+            # ==========================================
+            machine_id = live_data.get('machine_id', 'CNC_VIRTUAL_01')
             
+            # إنشاء النقطة (Point) التي سيتم حفظها في الداتابيز
+            point = (
+                Point("machine_health")
+                .tag("machine_id", machine_id)                     # Tags للفلترة
+                .tag("process", live_data.get('Machining_Process', 'Unknown'))
+                .field("prediction_stage", predicted_stage)        # حالة الأداة (0 إلى 3)
+                .field("confidence_percent", confidence)           # نسبة الثقة
+                .field("x1_current", float(live_data.get('X1_OutputCurrent', 0))) # تسجيل السنسور لتتبعه
+                .field("z1_current", float(live_data.get('Z1_OutputCurrent', 0)))
+                .time(time.time_ns(), WritePrecision.NS)           # الوقت اللحظي
+            )
+            
+            # الكتابة في InfluxDB
+            write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
    
             if predicted_stage == 3:
                 print(f"[CRITICAL ALERT] WORN OUT! Stop Machine! Conf: {confidence:.1f}%")
